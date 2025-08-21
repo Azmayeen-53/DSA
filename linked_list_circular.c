a4f1213dd6744301624f5020c8059cc9b1dbcf41
Azmayeen53 <nightnor53@gmail.com>
2025-08-22 00:34:40 +0600

    Add files via upload

--- linked_list_circular.c
+++ linked_list_circular.c
@@ -1,2 +1,42 @@
-#include<stdio.h>
-#include<stdlib.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+struct node
+{
+    int data;
+    struct node *next;
+};
+
+struct node *Linked_list_circular(struct node *head)
+{
+    struct node *ptr = head;
+
+    do
+    {
+        printf("%d ", ptr->data);
+        ptr = ptr->next;
+
+    } while (ptr != head);
+}
+int main()
+{
+
+    struct node *head;
+    struct node *FIRST;
+    struct node *SECOND;
+
+    head = (struct node *)malloc(sizeof(struct node));
+    FIRST = (struct node *)malloc(sizeof(struct node));
+    SECOND = (struct node *)malloc(sizeof(struct node));
+
+    head->data = 65;
+    head->next = FIRST;
+
+    FIRST->data = 23;
+    FIRST->next = SECOND;
+
+    SECOND->data = 213;
+    SECOND->next = head;
+
+    head = Linked_list_circular(head);
+}
